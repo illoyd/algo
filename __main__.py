f2f5fdd1284300e0bf5a5b6c82afe587a746fc3c
Ian Lloyd <ian.w.lloyd@gmail.com>
2017-10-26 20:46:42 +0800

    Change equity utilisation

--- __main__.py
+++ __main__.py
@@ -66,7 +66,7 @@ def main(args = {}):
 
   # Determine available captial to play with
   logging.info('STEP 4: CAPITAL')
-  capital = (client.equity() * 0.97) + client.margin()
+  capital = (client.equity() * 0.98) + client.margin()
   logging.info('Capital: %s', capital)
 
   # Get mid quotes
