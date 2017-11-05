145d8c8a40049c903f496dae489499ad9043b487
Ian Lloyd <ian.w.lloyd@gmail.com>
2017-11-05 14:18:05 +0800

    Working on resourceful framework

--- resourceful.py
+++ resourceful.py
@@ -5,6 +5,24 @@ import requests
 import json
 import simpleapi
 
+
+##
+# Collection!
+class Collection(object):
+  def __init__(self, api, base_uri):
+    self.api = api
+    self.base_uri = base_uri
+
+  def list(self):
+    if self._list is None:
+      self._list = self.get_list()
+    return self._list
+
+  def get_list(self):
+    return self.api.get(self.base_uri)
+
+
+
 ##
 # Resource!
 class Resource(object):
