3758da62baeb0ca33106823cfef46f6ad9358fd3
Ian Lloyd <ian.w.lloyd@gmail.com>
2017-11-20 08:30:00 +0800

    New WatchlistSharpeAlgo

--- algo.py
+++ algo.py
@@ -12,9 +12,94 @@ import dateutil
 import re
 import logging
 
+import helper
+
+
+##
+# Algo base, for working with signals and other items
+class Algo(object):
+
+  ##
+  # Initialise with a client object
+  def __init__(self, client):
+    self.client = client
+
+
 ##
 # Sharpe Algo class - for handling the calculations!
-class SharpeAlgo(object):
+class WatchlistSharpeAlgo(Algo):
 
-  def optimise(self, prices):
-    pass
+  ##
+  # Initialise with additional parameters for a Sharpe algo
+  def __init__(self, client, lookback = 21):
+    super().__init__(client)
+    self.lookback = lookback
+
+  ##
+  # Optimise
+  def optimise(self):
+    u = self.universe()
+    p = self.prices(u)
+    w = self.weights(p)
+    return w
+
+  ##
+  # Get the universe of stocks from the Robinhood Watchlist
+  # @returns A list of symbols
+  def universe(self):
+    universe = self.client.watchlist().symbols()
+    logging.info('Found %s', ', '.join(universe))
+    return universe
+
+  ##
+  # Get all prices for the given universe
+  # @returns A pandas dataframe of prices; vertical axis are dates and horizontal axis are symbols
+  def prices(self, universe):
+    prices = self.client.historical_prices(*universe).iloc[-self.lookback:]
+    logging.info('Found prices %s - %s for %s', prices.index[0], prices.index[-1], ", ".join(list(prices.columns)))
+    logging.debug(prices)
+    return prices
+
+  def weights(self, prices):
+    target_weights = self._calculate_target_weights(prices)
+    logging.info('Target weights: %s', ', '.join([ '{}: {:0.1f}%'.format(s, w * 100.0) for s, w in target_weights.iteritems() ]))
+    logging.debug(target_weights.round(2))
+    return target_weights
+
+  ##
+  # Calculate the optimal Sharpe portfolio
+  def _calculate_target_weights(self, prices):
+    prices = prices.astype(float)
+    best_weights, best_sharpe, best_days = None, 0.0, 0
+
+    collected_weights = []
+
+    while len(prices.index) >= 3:
+      weights, sharpe = self._calculate_target_weights_inner(prices)
+      weights['(SHARPE)'] = sharpe
+      weights.name = len(prices.index)
+      collected_weights.append(weights)
+
+      if sharpe >= best_sharpe:
+        best_weights, best_sharpe, best_days = weights, sharpe, weights.name
+      prices = prices[1:]
+
+    logging.debug(pd.concat(collected_weights, axis=1).round(2))
+    logging.debug('Best days %i', best_days)
+
+    return best_weights.drop('(SHARPE)')
+
+  def _calculate_target_weights_inner(self, prices):
+    # Perform general calculations
+    expected_returns = (prices.iloc[-1] / prices.iloc[0]) - 1
+    returns = prices.pct_change()
+    covariance = returns.cov()
+
+    # Run the tangency optimiser; on failure, return an empty series and a 0 sharpe.
+    try:
+        w = helper.tangency_portfolio(covariance, expected_returns)
+        return (w, helper.annualized_sharpe(returns, covariance, w))
+
+    except ValueError as e:
+        logging.error(e)
+        return (pd.Series(), 0.0)
--- helper.py
+++ helper.py
@@ -1,4 +1,15 @@
+# Standard library imports
+import sys
+import numpy as np
+import pandas as pd
+import math
+import cvxopt as opt
+import cvxopt.solvers as optsolvers
 import re
+
+##
+# A symbol look-up table. Not used yet.
+symbol_table = {}
 
 ##
 # Take the first non-None value from a given list
@@ -14,4 +25,82 @@ def id_for(sz):
   else:
     return None
 
-symbol_table = {}
+##
+# Calcalate the annualised Sharpe value for a given set of returns, covariances, and portfolio weights.
+# @returns A pandas.Series of expected or actual returns, with the index the symbol
+# @covariance A pandas.DataFrame of expected or actual covariance, with the indices being symbols
+# @weights A pandas.Series representing the relative weight or fractional holding of the symbol in the portfolio
+# @return A float representing the calculated annualized Sharpe value
+def annualized_sharpe(returns, covariance, weights):
+  portfolio_return = np.sum(returns.mean() * weights) * 252
+  portfolio_stddev = np.sqrt( np.dot( weights.T, np.dot(covariance, weights) ) * np.sqrt(252) )
+  return portfolio_return / portfolio_stddev
+
+##
+# Calculate a tangency portfolio
+# TODO See if we can replace with simple matrix algebra
+def tangency_portfolio(cov_mat, exp_rets, allow_short=False):
+  """
+  Computes a tangency portfolio, i.e. a maximum Sharpe ratio portfolio.
+
+  Note: As the Sharpe ratio is not invariant with respect
+  to leverage, it is not possible to construct non-trivial
+  market neutral tangency portfolios. This is because for
+  a positive initial Sharpe ratio the sharpe grows unbound
+  with increasing leverage.
+
+  Parameters
+  ----------
+  cov_mat: pandas.DataFrame
+      Covariance matrix of asset returns.
+  exp_rets: pandas.Series
+      Expected asset returns (often historical returns).
+  allow_short: bool, optional
+      If 'False' construct a long-only portfolio.
+      If 'True' allow shorting, i.e. negative weights.
+  Returns
+  -------
+  weights: pandas.Series
+      Optimal asset weights.
+  """
+  if not isinstance(cov_mat, pd.DataFrame):
+      raise ValueError("Covariance matrix is not a DataFrame")
+
+  if not isinstance(exp_rets, pd.Series):
+      raise ValueError("Expected returns is not a Series")
+
+  if not cov_mat.index.equals(exp_rets.index):
+      raise ValueError("Indices do not match")
+
+  n = len(cov_mat)
+
+  P = opt.matrix(cov_mat.values)
+  q = opt.matrix(0.0, (n, 1))
+
+  # Constraints Gx <= h
+  if not allow_short:
+      # exp_rets*x >= 1 and x >= 0
+      G = opt.matrix(np.vstack((-exp_rets.values,
+                                -np.identity(n))))
+      h = opt.matrix(np.vstack((-1.0,
+                                np.zeros((n, 1)))))
+  else:
+      # exp_rets*x >= 1
+      G = opt.matrix(-exp_rets.values).T
+      h = opt.matrix(-1.0)
+
+  # Solve
+  optsolvers.options['show_progress'] = False
+  sol = optsolvers.qp(P, q, G, h)
+
+  # Put weights into a labeled series
+  weights = pd.Series(sol['x'], index=cov_mat.index)
+
+  # Log warning on convergence issue
+  if sol['status'] != 'optimal':
+      logging.warn(weights)
+      raise ValueError("Convergence problem")
+
+  # Rescale weights, so that sum(weights) = 1
+  weights /= weights.sum()
+  return weights
